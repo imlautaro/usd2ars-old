633288824ed3be4e343f072de68da8e1f3b01519
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2020-12-30 13:27:25 -0300

    the appbar was raised to be above the content

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col h-screen w-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
-        <div class="flex items-center justify-between p-4 shadow bg-green-500 text-white">
+        <div class="flex items-center z-30 justify-between p-4 shadow bg-green-500 text-white">
             <octicons name="flame" />
             <span class="text-lg font-bold">USD2ARS</span>
             <button @click="fetchData()" class="focus:outline-none" :class="data == null ? 'animate-spin' : ''">
